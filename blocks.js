354331fc31eaffc0aebc2cd286d3e9b44d0fe46c
msmolich <110144057+msmolich@users.noreply.github.com>
2022-07-27 19:17:33 -0700

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,5 +1,4 @@
 function _blockGmailAds() {
-    console.log("!");
     var ads = document.querySelectorAll("span.ast");
     for (var x = 0; x < ads.length; ++x) {
         if (ads[x].innerHTML == 'Ad') {
@@ -11,11 +10,9 @@ function _blockGmailAds() {
 }
 
 window.addEventListener("load", function () {
-	console.log("2");
     var elementsToObserve = document.querySelectorAll("div.ae4");
     var observer = new MutationObserver(_blockGmailAds);
     for (var x = 0; x < elementsToObserve.length; ++x) {
-        console.log("3");
         observer.observe(elementsToObserve[x], {
             characterData: false,
             childList: true,
